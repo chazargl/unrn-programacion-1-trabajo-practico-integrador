549a958ad821ae31e3246f3d353a38776d6bd779
julietaguzman <168879318+julietaguzman@users.noreply.github.com>
2026-09-01 09:44:52 -0300

    Cambios

--- adaptar_datos.py
+++ adaptar_datos.py
@@ -1,7 +1,52 @@
 
-with open("assets/datos/observaciones.txt", "r", encoding="latin-1") as archivo:
-    for linea in archivo:
-        print(linea.strip())
+import sys
+import os
+import json
+import validaciones
+
+def procesar_txt(ruta_txt):
+    registros_validos = []
+    registros_invalidos = []
+
+    try:
+        with open(ruta_txt, "r", encoding="latin-1") as archivo:
+            for numero_linea, linea in enumerate(archivo, start = 1):
+                if validaciones.encabezado_o_vacia(linea):
+                    continue
+                registro, error = validaciones.parsear_linea(linea)
+
+                if registro is not None:
+                    registros_validos.append(registro)
+                else:
+                    registros_invalidos.append({
+                        "numero_de_linea": numero_linea,
+                        "linea_original": linea,
+                        "motivo_del_error": error
+                    })
+    except FileNotFoundError:
+        print(f"No se encontro el archivo: {ruta_txt}")
+        sys.exit(1)
+
+def generar_json(registros_validos, registros_invalidos, ruta_salida):
+
+    total = len(registros_validos) + len(registros_invalidos)
+    datos_json = {
+        "total_de_registros": total,
+        "cantidad_registros_validos": len (registros_validos),
+        "cantidad_registros_invalidos": len(registros_invalidos),
+        "registros_validos": registros_validos,
+        "registros_invalidos": registros_invalidos 
+    }
+
+def main():
+    if len(sys.argv) != 3:
+        print("Ingreso de datos: python adaptar_datos.py archivo_entrada.txt archivo_salida.json")
+
+    ruta_entrada = sys.argv[1]
+    ruta_salida = sys.argv[2]
+
+    registros_validos, registros_invalidos = procesar_txt(ruta_entrada)
+    generar_json(registros_validos, registros_invalidos, ruta_salida)
 
 # El parámetro encoding="latin-1" le indica a Python qué tabla de caracteres utilizar
 # para traducir los bytes crudos del archivo de texto a letras y símbolos legibles.
--- validaciones.py
+++ validaciones.py
@@ -1,47 +1,97 @@
 
 # Modulo de validaciones
 
-def encabezado_o_vacia (linea: str) -> bool:
+palabras_encabezado = ["FECHA", "HORA", "TEMP", "HUM", "PNM", "DD", "FF", "NOMBRE"]
+
+def encabezado_o_vacia (linea):
+    if not linea or not linea.strip():
+        return True
+    
+    linea_mayusculas = linea.upper()
+    for palabra in palabras_encabezado:
+        if palabra in linea_mayusculas:
+            return True
+
+    return False
+
+def validar_fecha (fecha_string):
+    if len(fecha_string) != 8:
+        return False, f"La fecha debe tener 8 digitos (DDMMAAAA): {fecha_string}"
+
+    if not fecha_string.isdigit():
+        return False, f"La fecha solo debe contener numeros: {fecha_string}"
+
+    try:
+        dia = int(fecha_string[0:2]) # [inicio, fin] abarca los dos primeros digitos
+        mes = int(fecha_string[2:4])
+        año = int(fecha_string[4:8])
+    except ValueError:
+        return False, f"Error al procesar la fecha:{fecha_string}"
 
 # Parsear: Analizar una cadena de texto de una sola línea 
 # para separar sus partes y extraer datos útiles
+def parsear_linea (linea):
+    linea_limpia = linea.strip()
+    if not linea_limpia:
+        return None, "Linea vacia"
 
-def validar_fecha (fecha_string: str) -> bool:
+    partes = linea_limpia.split() # Separa la lineas con espacios
 
-# Parsear: Analizar una cadena de texto de una sola línea 
-# para separar sus partes y extraer datos útiles
-def parsear_linea (linea: str):
+    if len(partes) < 8:
+        return None, "Faltan campos"
+
+    try:
+        fecha_string = partes[0]
+        hora_string = partes[1]
+        temp_string = partes[2]
+        hum_string = partes[3]
+        pnm_string = partes[4]
+        dd_string = partes[5]
+        ff_string = partes[6]
+        estacion = " ".join(partes[7:]) # Toma los elementos del indice 7 hasta el final
+        # " ".join(...): Une todos los elementos de esa sublista en un 
+        # único texto (string),insertando un espacio en blanco entre cada palabra.
+    except ValueError:
+        return None, "Contienen caracteres invalidos o faltan datos"
+
+    fecha_valida, error_fecha = validar_fecha(fecha_string)
+    if not fecha_valida:
+        return None, f"Error en fecha: {error_fecha}"
 
     try:
         hora = int(hora_string)
         temp = float(temp_string)
-        humedad = int(hum_string)
+        hum = int(hum_string)
         pnm = float(pnm_string)
         dd = int(dd_string)
-        ff = int(ff_string)
-    except ValueError:
-        return None, "Contienen caracteres invalidos o faltan datos."
-    
-    if hora <= 0 or hora >= 23:
-        return None, f"Hora fuera de rango (0 - 23): {hora}"
-    if humedad <= 0 or humedad >= 100:
-        return None, f"Humedad fuera de rango (0 - 100%) {humedad}"
-    if dd <= 0 or dd >= 360:
-        return None, f"Direccion del viento fuera del rango (0 - 360 grados): {dd}"
-    if ff < 0:
-        return None, f"Velocidad del viento negativa: {ff}"
-    if not estacion.strip():
-        return None, "El nombre de la estacion esta vacio."
+        ff = float(ff_string)
 
-    valido = {
+        if hora <= 0 or hora >= 23:
+            return None, f"Hora fuera de rango (0 - 23): {hora}"
+        if hum <= 0 or hum >= 100:
+            return None, f"Humedad fuera de rango (0 - 100%) {hum}"
+        if dd <= 0 or dd >= 360:
+            return None, f"Direccion del viento fuera del rango (0 - 360 grados): {dd}"
+        if ff < 0:
+            return None, f"Velocidad del viento negativa: {ff}"
+        if not estacion.strip():
+            return None, "El nombre de la estacion esta vacion"
+    except ValueError:
+        return None, "Contiene caracteres invalidos o faltan datos"
+
+    registro = {
         "fecha": fecha_string,
         "hora": hora,
         "temperatura": temp,
-        "humedad": humedad,
+        "humedad": hum,
         "presion": pnm,
         "direccion_del_viento": dd,
         "velocidad_del_viento": ff,
         "estacion": estacion
     }
+
+    return registro, None
+
+
 
     
